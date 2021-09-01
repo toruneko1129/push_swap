604d9898fcc96428b33cabfc2574c4070e34e1ca
toruneko1129 <kawakita.hayato@gmail.com>
2021-09-01 15:12:35 +0900

    make checker part

--- srcs/checker.c
+++ srcs/checker.c
@@ -6,11 +6,14 @@
 /*   By: hkawakit <hkawakit@student.42tokyo.j>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 01:12:42 by hkawakit          #+#    #+#             */
-/*   Updated: 2021/08/31 01:12:53 by hkawakit         ###   ########.fr       */
+/*   Updated: 2021/09/01 15:03:55 by hkawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(void)
+#include "../includes/push_swap.h"
+
+int	main(int argc, char **argv)
 {
+	check_argv(argc, argv);
 	return (0);
 }
