5750319c4bad43d95a07e26a34bdcd1558a31f47
toruneko1129 <kawakita.hayato@gmail.com>
2021-09-02 23:41:01 +0900

    updated checker

--- srcs/checker.c
+++ srcs/checker.c
@@ -6,7 +6,7 @@
 /*   By: hkawakit <hkawakit@student.42tokyo.j>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 01:12:42 by hkawakit          #+#    #+#             */
-/*   Updated: 2021/09/02 14:01:03 by hkawakit         ###   ########.fr       */
+/*   Updated: 2021/09/02 23:38:11 by hkawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,14 @@ int	main(int argc, char **argv)
 {
 	t_dlst	*stack_a;
 	t_dlst	*stack_b;
+	t_list	*cmds;
 
 	check_argv(argc, argv);
 	stack_a = dlstnew(NIL);
 	stack_b = dlstnew(NIL);
+	cmds = NULL;
 	check_init_failed(&stack_a, &stack_b);
-	free_all(&stack_a, &stack_b);
+	load_stack(argc, argv, &stack_a, &stack_b);
+	free_all(&stack_a, &stack_b, &cmds);
 	return (0);
 }
