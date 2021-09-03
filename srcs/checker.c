a72ffd0ea1515c86d892d8e418d524eac4a74762
toruneko1129 <kawakita.hayato@gmail.com>
2021-09-03 16:59:55 +0900

    make checker

--- srcs/checker.c
+++ srcs/checker.c
@@ -6,7 +6,7 @@
 /*   By: hkawakit <hkawakit@student.42tokyo.j>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 01:12:42 by hkawakit          #+#    #+#             */
-/*   Updated: 2021/09/02 23:38:11 by hkawakit         ###   ########.fr       */
+/*   Updated: 2021/09/03 16:56:58 by hkawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,15 @@ int	main(int argc, char **argv)
 {
 	t_dlst	*stack_a;
 	t_dlst	*stack_b;
-	t_list	*cmds;
+	t_dlst	*cmds;
 
 	check_argv(argc, argv);
 	stack_a = dlstnew(NIL);
 	stack_b = dlstnew(NIL);
-	cmds = NULL;
-	check_init_failed(&stack_a, &stack_b);
+	check_init_failed_stack(&stack_a, &stack_b);
 	load_stack(argc, argv, &stack_a, &stack_b);
+	cmds = dlstnew(NIL);
+	check_init_failed_cmds(&stack_a, &stack_b, &cmds);
 	free_all(&stack_a, &stack_b, &cmds);
 	return (0);
 }
