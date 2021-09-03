5c9ca4ef5e44d137c5ee2a8ea6e230ec4b2be2c3
toruneko1129 <kawakita.hayato@gmail.com>
2021-09-03 21:55:36 +0900

    created pushcmd

--- srcs/exec_command.c
+++ srcs/exec_command.c
@@ -6,7 +6,7 @@
 /*   By: hkawakit <hkawakit@student.42tokyo.j>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 17:48:45 by hkawakit          #+#    #+#             */
-/*   Updated: 2021/09/03 21:33:02 by hkawakit         ###   ########.fr       */
+/*   Updated: 2021/09/03 21:45:16 by hkawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,27 @@ static void	exec_cmd_swap(t_dlst **stack)
 	(*stack)->next->val = tmp;
 }
 
+static void	exec_cmd_push(t_dlst **stack_dst, t_dlst **stack_src)
+{
+	t_dlst	*top;
+
+	if ((*stack_src)->val == NIL)
+		return ;
+	top = *stack_src;
+	*stack_src = (*stack_src)->next;
+	top->prev->next = top->next;
+	top->next->prev = top->prev;
+	dlstadd_front(stack_dst, top);
+}
+
 void	exec_command(int type, t_dlst **stack_a, t_dlst **stack_b)
 {
 	if (type == SA || type == SS)
 		exec_cmd_swap(stack_a);
 	if (type == SB || type == SS)
 		exec_cmd_swap(stack_b);
+	if (type == PA)
+		exec_cmd_push(stack_a, stack_b);
+	if (type == PB)
+		exec_cmd_push(stack_b, stack_a);
 }
--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: hkawakit <hkawakit@student.42tokyo.j>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 21:59:40 by hkawakit          #+#    #+#             */
-/*   Updated: 2021/09/03 21:35:59 by hkawakit         ###   ########.fr       */
+/*   Updated: 2021/09/03 21:54:43 by hkawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,23 @@ int	main(int argc, char **argv)
 	cmds = dlstnew(NIL);
 	check_init_failed_cmds(&stack_a, &stack_b, &cmds);
 	//solve(&stack_a, &stack_b, &cmds);
+	//debug mode
 	dbg_dlst(stack_a, "a");
+	dbg_dlst(stack_b, "b");
+	ft_putchar_fd('\n', STDOUT);
+	exec_command(PA, &stack_a, &stack_b);
 	exec_command(SA, &stack_a, &stack_b);
 	dbg_dlst(stack_a, "a");
+	dbg_dlst(stack_b, "b");
+	ft_putchar_fd('\n', STDOUT);
+	rep(i, 3)
+	{
+		exec_command(PB, &stack_a, &stack_b);
+	}
+	dbg_dlst(stack_a, "a");
+	dbg_dlst(stack_b, "b");
+	ft_putchar_fd('\n', STDOUT);
+	//debug end
 	free_all(&stack_a, &stack_b, &cmds);
 	return (0);
 }
